472722ed8dde21fd19bd4aa0e22408475c9cbcff
nikpr <prohorovnick@gmail.com>
2018-03-29 15:48:12 +0300

    add logging feature

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -3,8 +3,11 @@ package app;
 import argument_parser.ArgumentParser;
 import city.City;
 import greet_generator.Greeter;
+import org.apache.log4j.Logger;
 
 public class App {
+
+    private static final Logger logger = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
         ArgumentParser parser = new ArgumentParser();
@@ -13,10 +16,11 @@ public class App {
         try {
             city = parser.parseArguments(args);
             greeter.greet(city);
+            logger.info("Greetings for: " + city.getCityName());
         } catch (Exception ex) {
-           
+            logger.error(ex);
         }
-        
+
     }
 
 }
